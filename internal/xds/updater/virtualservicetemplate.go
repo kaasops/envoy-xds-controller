c384e085e221dee0cc0af57e9bdc179adb2b18a9
Aleksandr Aleksandrov <aaleksandrov.cy@gmail.com>
2025-10-02 17:09:06 +0300

    small fixes, debug logs, update ci

--- internal/xds/updater/virtualservicetemplate.go
+++ internal/xds/updater/virtualservicetemplate.go
@@ -2,15 +2,24 @@ package updater
 
 import (
 	"context"
+	"time"
 
 	"github.com/kaasops/envoy-xds-controller/api/v1alpha1"
 	"github.com/kaasops/envoy-xds-controller/internal/helpers"
 	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 func (c *CacheUpdater) ApplyVirtualServiceTemplate(ctx context.Context, vst *v1alpha1.VirtualServiceTemplate) (isUpdate bool) {
+	lockStart := time.Now()
 	c.mx.Lock()
 	defer c.mx.Unlock()
+	lockAcquireDuration := time.Since(lockStart)
+	if lockAcquireDuration > 100*time.Millisecond {
+		rlog := log.FromContext(ctx).WithName("cache-updater")
+		rlog.Info("ApplyVST: lock contention detected", "template", vst.Name, "lockWaitDuration", lockAcquireDuration.String())
+	}
+
 	prevVST := c.store.GetVirtualServiceTemplate(helpers.NamespacedName{Namespace: vst.Namespace, Name: vst.Name})
 	if prevVST == nil {
 		c.store.SetVirtualServiceTemplate(vst)
@@ -26,8 +35,15 @@ func (c *CacheUpdater) ApplyVirtualServiceTemplate(ctx context.Context, vst *v1a
 }
 
 func (c *CacheUpdater) DeleteVirtualServiceTemplate(ctx context.Context, nn types.NamespacedName) error {
+	lockStart := time.Now()
 	c.mx.Lock()
 	defer c.mx.Unlock()
+	lockAcquireDuration := time.Since(lockStart)
+	if lockAcquireDuration > 100*time.Millisecond {
+		rlog := log.FromContext(ctx).WithName("cache-updater")
+		rlog.Info("DeleteVST: lock contention detected", "template", nn.String(), "lockWaitDuration", lockAcquireDuration.String())
+	}
+
 	if !c.store.IsExistingVirtualServiceTemplate(helpers.NamespacedName{Namespace: nn.Namespace, Name: nn.Name}) {
 		return nil
 	}
